a47b700b6561d511c6703cd659713bec57d997e0
EuiYul Song <eysong@uw.edu>
2026-09-05 19:05:05 +0900

    Update run.sh to add memory_squad_chunk_ablation.py

--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@ python3 memory_ablation_3exp.py \
   --max-summary-updates 2 \
   --prompt-limit-words 200 \
   --truncate-tokens 128
+python3 memory_squad_chunk_ablation.py \
+  --limit 50 \
+  --workers 20 \
+  --prompt-limit-words 100
